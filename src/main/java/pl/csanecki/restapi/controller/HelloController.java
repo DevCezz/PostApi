381f299232dfd3641c3c96110c2ad27668064a8d
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-05-21 17:14:54 +0200

    Wstrzyknięcie serwisu i wywołanie jego metody

--- src/main/java/pl/csanecki/restapi/controller/HelloController.java
+++ src/main/java/pl/csanecki/restapi/controller/HelloController.java
@@ -2,12 +2,19 @@ package pl.csanecki.restapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.csanecki.restapi.service.HelloService;
 
 @RestController
 public class HelloController {
 
+    private HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
     @GetMapping("/")
     public String hello() {
-        return "Hello World!";
+        return helloService.hello();
     }
 }

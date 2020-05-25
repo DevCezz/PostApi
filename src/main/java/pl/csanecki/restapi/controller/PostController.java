733cc23b3fb10e358d974d017628c49b5944e9bb
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-05-25 18:51:55 +0200

    Pobieranie jedno POST po id

--- src/main/java/pl/csanecki/restapi/controller/PostController.java
+++ src/main/java/pl/csanecki/restapi/controller/PostController.java
@@ -1,6 +1,7 @@
 package pl.csanecki.restapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pl.csanecki.restapi.model.Post;
 
@@ -11,6 +12,11 @@ public class PostController {
 
     @GetMapping("/posts")
     public List<Post> getPosts() {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    @GetMapping("/posts/{postId}")
+    public Post getSinglePost(@PathVariable long postId) {
         throw new UnsupportedOperationException("Not implemented yet!");
     }
 }

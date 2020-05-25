bfb68175697916547aa73e1382c3f1ce897b1578
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-05-25 19:07:45 +0200

    Wstrzyknięcie repository do serwisu POST

--- src/main/java/pl/csanecki/restapi/service/PostService.java
+++ src/main/java/pl/csanecki/restapi/service/PostService.java
@@ -1,12 +1,17 @@
 package pl.csanecki.restapi.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.csanecki.restapi.model.Post;
+import pl.csanecki.restapi.repository.PostRepository;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class PostService {
+
+    private final PostRepository postRepository;
 
     public List<Post> getPosts() {
         throw new UnsupportedOperationException("Not implemented yet!");

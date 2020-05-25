23915138b51548fb03e9eb68f1b0ce93632e12e0
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-05-25 18:44:08 +0200

    Dodanie adotacji Enitity dla POST

--- src/main/java/pl/csanecki/restapi/model/Post.java
+++ src/main/java/pl/csanecki/restapi/model/Post.java
@@ -3,8 +3,10 @@ package pl.csanecki.restapi.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
 import java.time.LocalDateTime;
 
+@Entity
 @Getter
 @Setter
 public class Post {

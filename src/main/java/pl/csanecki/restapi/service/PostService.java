aacdbda010f88fb94a85b595ef70c05afa51b4ad
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-05-25 19:05:40 +0200

    Dodanie metody do pobierania POSTS

--- src/main/java/pl/csanecki/restapi/service/PostService.java
+++ src/main/java/pl/csanecki/restapi/service/PostService.java
@@ -1,8 +1,14 @@
 package pl.csanecki.restapi.service;
 
 import org.springframework.stereotype.Service;
+import pl.csanecki.restapi.model.Post;
+
+import java.util.List;
 
 @Service
 public class PostService {
 
+    public List<Post> getPosts() {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
 }

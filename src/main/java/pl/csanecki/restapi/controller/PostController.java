5f2e652e7115ae79e3e4ee9c5ae7b9f05cd6a30b
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-05-25 18:46:43 +0200

    Obsługa klasy kontolera - sygnatura dla zwracania POSTS

--- src/main/java/pl/csanecki/restapi/controller/PostController.java
+++ src/main/java/pl/csanecki/restapi/controller/PostController.java
@@ -1,4 +1,16 @@
 package pl.csanecki.restapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.csanecki.restapi.model.Post;
+
+import java.util.List;
+
+@RestController
 public class PostController {
+
+    @GetMapping("/posts")
+    public List<Post> getPosts() {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
 }

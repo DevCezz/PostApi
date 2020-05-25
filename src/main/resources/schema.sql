0f92e043c015df24bcac4c1f3e36d9e76e337c67
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-05-25 17:04:38 +0200

    Dodanie związku 1:n między POST a COMMENT

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,3 +11,7 @@ CREATE TABLE COMMENT (
     content VARCHAR(2000) NULL,
     created TIMESTAMP
 );
+
+ALTER TABLE COMMENT
+    ADD CONSTRAINT comment_post_id
+    FOREIGN KEY (post_id) REFERENCES post(id);

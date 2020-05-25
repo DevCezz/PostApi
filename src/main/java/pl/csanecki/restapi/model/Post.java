33da413a75e6a441b3accc2808fbcaf00cc39de9
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-05-25 18:43:39 +0200

    Dodanie pól do klasy modelu POST

--- src/main/java/pl/csanecki/restapi/model/Post.java
+++ src/main/java/pl/csanecki/restapi/model/Post.java
@@ -1,5 +1,16 @@
 package pl.csanecki.restapi.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class Post {
 
+    private long id;
+    private String title;
+    private String content;
+    private LocalDateTime created;
 }

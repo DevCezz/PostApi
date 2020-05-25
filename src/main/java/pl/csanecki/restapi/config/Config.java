57b3b91517eb5afc006193f6da879b74d917e6c4
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-05-25 19:00:30 +0200

    Przeniesienie adnotacji swagger'a

--- src/main/java/pl/csanecki/restapi/config/Config.java
+++ src/main/java/pl/csanecki/restapi/config/Config.java
@@ -1,8 +1,10 @@
 package pl.csanecki.restapi.config;
 
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class Config {
 
 }

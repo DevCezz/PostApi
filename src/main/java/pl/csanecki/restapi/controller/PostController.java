d3a851cfd178fc2777a05e5147a769f709e04000
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-05-25 19:10:35 +0200

    Wstrzyknięcie serwisu do konrolera POST

--- src/main/java/pl/csanecki/restapi/controller/PostController.java
+++ src/main/java/pl/csanecki/restapi/controller/PostController.java
@@ -1,18 +1,23 @@
 package pl.csanecki.restapi.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pl.csanecki.restapi.model.Post;
+import pl.csanecki.restapi.service.PostService;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
 
     @GetMapping("/posts")
     public List<Post> getPosts() {
-        throw new UnsupportedOperationException("Not implemented yet!");
+        return postService.getPosts();
     }
 
     @GetMapping("/posts/{postId}")

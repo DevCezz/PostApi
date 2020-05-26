95c796af58cd1de04b452e6b1ad2af6940d2267a
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-05-26 18:44:52 +0200

    Dodanie zapytania rozwiązującego problem N+1

--- src/main/java/pl/csanecki/restapi/repository/PostRepository.java
+++ src/main/java/pl/csanecki/restapi/repository/PostRepository.java
@@ -1,9 +1,16 @@
 package pl.csanecki.restapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.csanecki.restapi.model.Post;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("select p from Post p" +
+            " left join fetch p.comment")
+    List<Post> findAllPosts();
 }

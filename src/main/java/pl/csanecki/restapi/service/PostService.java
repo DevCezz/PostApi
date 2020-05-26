2b3aeae6deccafd538c6ddb171d67edb7acf8450
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-05-26 18:48:07 +0200

    Przykładowe wywołanie stronnicowania

--- src/main/java/pl/csanecki/restapi/service/PostService.java
+++ src/main/java/pl/csanecki/restapi/service/PostService.java
@@ -1,6 +1,7 @@
 package pl.csanecki.restapi.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import pl.csanecki.restapi.model.Post;
 import pl.csanecki.restapi.repository.PostRepository;
@@ -14,7 +15,7 @@ public class PostService {
     private final PostRepository postRepository;
 
     public List<Post> getPosts() {
-        return postRepository.findAllPosts();
+        return postRepository.findAllPosts(PageRequest.of(0, 5));
     }
 
     public Post getSinglePost(long id) {

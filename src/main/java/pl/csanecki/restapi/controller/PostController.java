ab7674efcc130fa658beb247c6e145f387fe7be3
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-05-27 12:13:43 +0200

    Dodanie mapowanie na POST dto

--- src/main/java/pl/csanecki/restapi/controller/PostController.java
+++ src/main/java/pl/csanecki/restapi/controller/PostController.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import pl.csanecki.restapi.dto.PostDto;
 import pl.csanecki.restapi.model.Post;
 import pl.csanecki.restapi.service.PostService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +18,23 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/posts")
-    public List<Post> getPosts() {
-        return postService.getPosts();
+    public List<PostDto> getPosts() {
+        return mapToPostDtos(postService.getPosts());
+    }
+
+    private List<PostDto> mapToPostDtos(List<Post> posts) {
+        return posts.stream()
+                .map(this::mapToPostDto)
+                .collect(Collectors.toList());
+    }
+
+    private PostDto mapToPostDto(Post post) {
+        return PostDto.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .created(post.getCreated())
+                .build();
     }
 
     @GetMapping("/posts/{postId}")

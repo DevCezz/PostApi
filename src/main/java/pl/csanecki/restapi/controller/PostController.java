f391f515b1e8b3e92bf84f0a1390c0c8f2d50d66
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-05-27 12:38:43 +0200

    Dodanie parametru do stronnicowania

--- src/main/java/pl/csanecki/restapi/controller/PostController.java
+++ src/main/java/pl/csanecki/restapi/controller/PostController.java
@@ -3,6 +3,7 @@ package pl.csanecki.restapi.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.csanecki.restapi.dto.PostDto;
 import pl.csanecki.restapi.model.Post;
@@ -17,8 +18,9 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/posts")
-    public List<PostDto> getPosts() {
-        return PostDtoMapper.mapToPostDtos(postService.getPosts());
+    public List<PostDto> getPosts(@RequestParam(required = false, defaultValue = "0") int page) {
+        int requestedPage = page > 0 ? page : 0;
+        return PostDtoMapper.mapToPostDtos(postService.getPosts(requestedPage));
     }
 
     @GetMapping("/posts/{postId}")

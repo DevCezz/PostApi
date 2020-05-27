2247da8829e33edda0c7fdb5b213bf83a07f4959
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-05-27 18:23:21 +0200

    Dodanie adnotacji do generowania id POST

--- src/main/java/pl/csanecki/restapi/model/Post.java
+++ src/main/java/pl/csanecki/restapi/model/Post.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
@@ -16,6 +18,7 @@ import java.util.List;
 public class Post {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private String content;
